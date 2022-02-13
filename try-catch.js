f89c470074d6ddd376f58222fd207fc5aae9e2ee
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-14 01:27:39 +0600

    Different types of Error object and explore Try-catch

---
+++ try-catch.js
@@ -0,0 +1,16 @@
+const myName = 'ami';
+const student = undefined;
+// console.log(student.name);
+// for(let i = 0; i < 10 i++){
+
+// }
+
+//  console.log(name);
+ try{
+    myName = 'tumi'
+ }
+ catch(error){
+    console.log('getting error',error);
+ }
+ console.log(100);
+ console.log(myName);
